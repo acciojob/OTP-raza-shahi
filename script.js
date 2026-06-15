643ed3bcbb69b1b2f09e5a8be7690674233d234a
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-06-15 18:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const inputs = document.getElementById("code-container");
+
+inputs.addEventListener("input", (e) => {
+  const target = e.target;
+  const value = target.value;
+  if (isNaN(value)) {
+    target.value = "";
+    return;
+  }
+
+  if (value != "") {
+    const next = target.nextElementSibling;
+    if (next) {
+      next.focus();
+    }
+  }
+});
+
+inputs.addEventListener("keyup", (e) => {
+  const target = e.target;
+  const key = e.key.toLowerCase();
+  if (key === "backspace" || key === "delete") {
+    target.value = "";
+    const prev = target.previousElementSibling;
+    if (prev) {
+      prev.focus();
+    }
+    return;
+  }
+});
